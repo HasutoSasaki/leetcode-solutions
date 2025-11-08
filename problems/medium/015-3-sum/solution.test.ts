2e4a38c8e066e9156d08b80bfeb4f0ad5258fb3d
HasutoSasaki <hasuto.sa@gmail.com>
2025-11-08 12:53:52 +0900

    fix(015): テストコードで配列を検証する際に方法を修正.

--- problems/medium/015-3-sum/solution.test.ts
+++ problems/medium/015-3-sum/solution.test.ts
@@ -2,10 +2,10 @@ import { threeSum } from "./solution";
 
 describe('threeSum', () => {
     it('case1', () => {
-        expect(threeSum([-1, 0, 1, 2, -1, -4])).toBe([[-1, -1, 2], [-1, 0, 1]])
+        expect(threeSum([-1, 0, 1, 2, -1, -4])).toEqual([[-1, -1, 2], [-1, 0, 1]])
     })
 
     it('case2', () => {
-        expect(threeSum([0, 1, 1])).toBe([])
+        expect(threeSum([0, 1, 1])).toEqual([])
     })
 })
